157dfb27d56d2793f695d039d5d7456bc8a0613f
Christian Smith <smith.christian.william@gmail.com>
2025-11-29 19:55:12 -0500

    use Pipfile instead

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd "${DIR}" > /dev/null
-pipenv install --target backend/python-env
-npm run dist
-popd "${DIR}" > /dev/null
